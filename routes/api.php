0e97afde4f65e577d60e58a2614b75bd73c9a3d1
Abraham <aagaleano@hotmail.com>
2020-05-09 12:54:07 -0300

    control en la api con un apikey

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::resource('certificado','ElCertificado');
+// Route::resource('certificado','ElCertificado');
+Route::group(["middleware" => "apikey.validate"], function () {
+
+    //Rutas
+    Route::post("login", "Api\UserController@postLogin");
+    Route::get("cursos", "Api\CursoController@getCursos");
+    Route::resource('certificado','ElCertificado');
+  
+  });
