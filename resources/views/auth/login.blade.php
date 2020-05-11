5fb4d114cc70aaac4e7fbb5a5d388f11168a5d5f
Abraham <aagaleano@hotmail.com>
2020-05-11 02:23:51 -0300

    mejoras en las vistas y add de PDF

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,13 +14,26 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+                        
+
                         <div class="form-group row">
+
+                            
+
+
                             <label for="email" class="col-md-4 col-form-label text-md-right">Usuario</label>
 
                             <div class="col-md-6">
-                                {{-- <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> --}}
-                                <input id="email" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
+                                
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    </div>
+                                    <input id="email" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
+    
+                                </div>
 
+                                
                                 @if ($errors->has('username'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('username') }}</strong>
@@ -33,14 +46,23 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">Clave</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                    </div>
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+    
+                                </div>
+                            </div>    
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            
+                            
                         </div>
 
                         <div class="form-group row">
